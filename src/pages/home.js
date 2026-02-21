81f66d086ae230014aacbad21d8e7c51358b35c0
Dawit <208569702+dawit-kibatu@users.noreply.github.com.>
2026-02-21 13:24:15 +0300

    remove slide show images

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,20 +5,6 @@ let home=function(){
     let forest=document.createElement("div");
     let bigImage=document.createElement("div");
     bigImage.id="bigImage";
-    let slideContainer=document.createElement("div");
-    let slide1=document.createElement("div");
-    let slide2=document.createElement("div");
-    let slide3=document.createElement("div");
-    let slide4=document.createElement("div");
-    slide1.classList.add("slide","slide1")
-    slide2.classList.add("slide","slide2")
-    slide3.classList.add("slide","slide3")
-    slide4.classList.add("slide","slide4")
-    slideContainer.appendChild(slide1);
-    slideContainer.appendChild(slide2);
-    slideContainer.appendChild(slide3);
-    slideContainer.appendChild(slide4);
-    bigImage.appendChild(slideContainer);
     let forestText=document.createElement("div");
     let forestImg=document.createElement("div");
     forestText.id="forestTxt";
